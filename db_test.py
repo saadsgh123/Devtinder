60f28779164403ec7d983d49e7da889a56e78f88
saadsgh123 <sdsghouri@gmail.com>
2024-09-07 21:13:56 +0100

    db test (list users) 5

--- db_test.py
+++ db_test.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     from models import storage
 
-    users = storage.all().values()
-    for k, v in users.iteritems():
+    users = storage.all()
+    for k, v in users.items():
         print(k, v)
 
