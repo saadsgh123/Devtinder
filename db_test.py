d6b64de20bf490bbff39371a476041f82d2925f3
saadsgh123 <sdsghouri@gmail.com>
2024-09-07 21:39:38 +0100

    db test (get user by job title) 1

--- db_test.py
+++ db_test.py
@@ -1,7 +1,12 @@
 if __name__ == '__main__':
     from models import storage
 
+    print("=========== Get user by its ID =============")
     user = storage.get_user_by_id(id=1)
     print(user.username)
+    print("=========== END =============")
 
-
+    print("=========== Get user by its JOB_TITLE =============")
+    user = storage.get_users_by_job_title("Full")
+    print(user.username)
+    print("=========== END =============")
