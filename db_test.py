9d3cb51c5f84503169c776d435aa6267e4205b3e
saadsgh123 <sdsghouri@gmail.com>
2024-09-07 21:43:09 +0100

    db test (get user by job title) 3

--- db_test.py
+++ db_test.py
@@ -3,11 +3,11 @@ if __name__ == '__main__':
 
     print("=========== Get user by its ID =============")
     user = storage.get_user_by_id(id=1)
-    print(user.username)
+    print(user.username, user.email, user.job_title)
     print("=========== END =============")
 
     print("=========== Get user by its JOB_TITLE =============")
     users = storage.get_users_by_job_title("Data")
     for user in users:
-        print(user.username)
+        print(user.username, user.email, user.job_title)
     print("=========== END =============")
