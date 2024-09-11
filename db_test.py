563e67a1e3234905edd3f5c462f7c2d09562f3f1
saadsgh123 <sdsghouri@gmail.com>
2024-09-11 10:25:34 +0100

    db test (create a new user) 8

--- db_test.py
+++ db_test.py
@@ -1,4 +1,4 @@
-
+from console.meta import email
 
 if __name__ == '__main__':
     from models import storage
@@ -39,16 +39,8 @@ if __name__ == '__main__':
     print("")
 
     print("=========== Create a New user =============")
-    data = {
-        "username": "sys.argv[1]",
-        "email": "sys.argv[2]",
-        "job_title": "sys.argv[3]",
-        "password": "sys.argv[4]",
-        "country": "sys.argv[5]",
-        "city": "sys.argv[6]",
-        "Exp": 1
-    }
-    storage.create_user_profile(data)
+
+    storage.create_user_profile(username=sys.argv[1], email=sys.argv[2], job_title=sys.argv[3], country=sys.argv[4], city=sys.argv[5])
     print(user.id, user.email, user.job_title)
     print("=========== END =============")
     print("")
