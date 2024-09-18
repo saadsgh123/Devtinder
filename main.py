b7f60597740930207f30822b20aacf619e77955f
saadsgh123 <sdsghouri@gmail.com>
2024-09-18 16:55:26 +0100

    security test 2

--- main.py
+++ main.py
@@ -75,7 +75,6 @@ def home():
                 return redirect(url_for('home'))
     else:
         return redirect(url_for('login'))
-    return render_template("main/home.html")
 
 
 @app.route("/feed", methods=['GET', 'POST'])
