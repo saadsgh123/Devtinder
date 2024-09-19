797f5beb3c5a3a9830ec1eeebd8ed19e605aa2b9
saadsgh123 <sdsghouri@gmail.com>
2024-09-19 13:58:45 +0100

    dbtest update

--- db_test.py
+++ db_test.py
@@ -1,22 +1,20 @@
-
 if __name__ == '__main__':
     from models import storage
     from models.School import School
     from models.User import User
-    import sys
 
     print("=========== Get all users =============")
     users = storage.all(User).values()
     print("return type =>", type(users))
     for user in users:
-        print(user.id, user.username, user.job_title)
+        print(user.id, user.username, user.job_title, user.email, user.password)
     print("=========== END =============")
     print("")
     print("=========== Get user by its ID =============")
     user = storage.get_user_by_id(id="123e4567-e89b-12d3-a456-426614174001")
     print("return type =>", type(user))
     if user:
-        print(user.id, user.email, user.job_title)
+        print(user.id, user.email, user.job_title, user.password)
     else:
         print("doesn't exists")
     print("=========== END =============")
@@ -40,15 +38,12 @@ if __name__ == '__main__':
     print("=========== END =============")
     print("")
 
-
-
     print("=========== Update user =============")
-    storage.update_user_profile(id="123e4567-e89b-12d3-a456-426614174001", username="sys.argv[1]", email="sys.argv[2]",
-                                job_title="sys.argv[3]", country="sys.argv[4]", city="sys.argv[5]", password="sys.argv[6]", exp=2)
+    try:
+        storage.update_user_profile(id="123e4567-e89b-12d3-a456-426614174001", username="sys.argv[1]",
+                                    email="sys.argv[2]", job_title="sys.argv[3]", country="sys.argv[4]",
+                                    city="sys.argv[5]", password="sys.argv[6]", exp=2)
+    except Exception as e:
+        print(e)
     print("=========== END =============")
     print("")
-
-
-
-
-
