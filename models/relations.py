eb9c33be22c5ac2032c1b9ce2086c1d036562d83
saadsgh123 <sdsghouri@gmail.com>
2024-09-22 13:01:40 +0100

    relation db test 3 🥶

--- models/relations.py
+++ models/relations.py
@@ -2,7 +2,7 @@ from models.basemodel import Base
 from sqlalchemy import Table, Column, String, ForeignKey
 
 # Association table for the many-to-many relationship
-user_tech_table = Table('post_tag', Base.metadata,
-                        Column('post_id', String(36), ForeignKey('posts.id')),
-                        Column('tag_id', String(36), ForeignKey('tags.id'))
+user_tech_table = Table('user_tech', Base.metadata,
+                        Column('user_id', String(36), ForeignKey('user.id')),
+                        Column('tech_id', String(36), ForeignKey('tech.id'))
                         )
