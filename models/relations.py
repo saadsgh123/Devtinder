0379a59cdb5ec42365f583ca2960895a9c35deb4
saadsgh123 <sdsghouri@gmail.com>
2024-09-22 13:35:05 +0100

    relation db test 5 🥶

--- models/relations.py
+++ models/relations.py
@@ -4,5 +4,5 @@ from sqlalchemy import Table, Column, String, ForeignKey
 # Association table for the many-to-many relationship
 user_tech_table = Table('user_tech', Base.metadata,
                         Column('user_id', String(36), ForeignKey('user.id')),
-                        Column('tech_id', String(36), ForeignKey('technology.id'))
+                        Column('tech_id', String(36), ForeignKey('tech.id'))
                         )
