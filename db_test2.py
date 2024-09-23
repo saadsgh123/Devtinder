4b7ff3c36a50f5584c5dd9149a46b7f19a5e251e
saadsgh123 <sdsghouri@gmail.com>
2024-09-23 16:25:10 +0100

    db test 4 (circular import issue)

--- db_test2.py
+++ db_test2.py
@@ -2,8 +2,8 @@
 from models import storage
 
 if __name__ == '__main__':
-    # Delay import of user_tech_table to avoid potential circular dependency
     from models.relations import user_tech_table
+    from models.User import User
 
     # Output for creating technologies
     print("=========== Create technologie =============")
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
     # Output for getting user skills from the user_tech_table
     print("=========== Get user skills =============")
-    tech_user = storage.all(user_tech_table).values()
+    tech_user = storage.all(User).values()
     for v in tech_user:
         print(v)
     print("=========== END =============\n")
