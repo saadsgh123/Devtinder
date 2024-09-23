a755e4043214208d4afcd14692723daa109474e5
saadsgh123 <sdsghouri@gmail.com>
2024-09-23 15:48:20 +0100

    db test 1 (skills)

--- db_test2.py
+++ db_test2.py
@@ -1,15 +1,26 @@
-
+from models.User import User
+from models.Technology import Technology
 
 if __name__ == '__main__':
-    from models import storage
-    from models.School import School
-    from models.User import User
-    from models.Technology import Technology
-
     print("=========== Create technologie =============")
-    user = User(username="saadsgh123", email="saadsgh123@gmail.com", password="saad")
-    tech1 = Technology(name="Python", picture="/static/images/python.png")
-    user.technology.append(tech1)
+    # user = User(username="saadsgh123", email="saadsgh123@gmail.com", password="saad")
+    # tech1 = Technology(name="Python", picture="/static/images/python.png")
+    # user.technology.append(tech1)
+    # user.save()
+    print("=========== END =============")
+    print("")
+
+    print("=========== add a list of skills & assign them a user =============")
+    skills = ["Python", "JavaScript", "HTML"]
+    user = User(username="saad", email="saad@gmail.com", password="saad")
+    for skill in skills:
+        tech = Technology(name=skill, picture="/static/images/python.png")
+        user.technology.append(tech)
     user.save()
     print("=========== END =============")
     print("")
+
+
+
+
+
